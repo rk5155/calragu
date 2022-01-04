b41b6b4a1ea9f71e3324c0a3b25a315399131d72
rk <>
2022-01-04 16:41:55 +0900

    header と main エリアを作成した

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+    <header id="header">
+      <nav class="navbar navbar-expand-sm navbar-light">
+        <h1 class="navbar-brand u-font__bold" to="/" exact>【自動でいろいろ計算できるサイト】コンピュテーションAI</h1>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav4" aria-controls="navbarNav4" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-end" id="navbarNav4">
+            <ul class="navbar-nav">
+                <li>jlksadfkj</li>
+                <li>jlksadfkj</li>
+                <li>jlksadfkj</li>
+            </ul>
+        </div>
+      </nav>
+
+    </header>
+</template>
---
+++ src/components/Main.vue
@@ -0,0 +1,30 @@
+<template>
+    <main>
+        <nav>
+            <ul>
+                <li>スポーツ</li>
+                <li>数学・算数</li>
+                <li>科学</li>
+                <li>健康</li>
+                <li>日付</li>
+                <li>お金</li>
+            </ul>
+        </nav>
+    </main>
+</template>
+
+<script>
+
+
+export default {
+  components: {
+  }
+}
+</script>
+
+<style scoped>
+main {
+    width: 1200px;
+    margin: 0 auto;
+}
+</style>
