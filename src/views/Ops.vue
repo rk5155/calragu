7a3a911f43daba33b763c04860911375e1ecde2b
rk <>
2022-01-07 15:36:25 +0900

    Ops.vue リファクタリングした

--- src/views/Ops.vue
+++ src/views/Ops.vue
@@ -2,56 +2,24 @@
     <div>
         <h2>OPS計算</h2>
         <form>
-            <div class="form-group">
-                <label for="formGroupExampleInput">打数</label>
-                <input v-model.number="bat" type="number" min="0" max="1000" step="1" class="form-control">
+            <div class="form-group" v-for="list in formList" :key="list.label">
+                <label for="formGroupExampleInput">{{ list.label }}</label>
+                <input v-model.number="$data[list.data]" type="number" min="0" max="1000" step="1" class="form-control">
             </div>
-            <div class="form-group">
-                <label for="formGroupExampleInput2">単打数</label>
-                <input v-model.number="hit1" type="number" min="0" max="1000" step="1" class="form-control">
-            </div>
-            <div class="form-group">
-                <label for="formGroupExampleInput2">二塁打数</label>
-                <input v-model.number="hit2" type="number" min="0" max="1000" step="1" class="form-control">
-            </div>
-            <div class="form-group">
-                <label for="formGroupExampleInput2">三塁打数</label>
-                <input v-model.number="hit3" type="number" min="0" max="1000" step="1" class="form-control">
-            </div>
-            <div class="form-group">
-                <label for="formGroupExampleInput2">本塁打数</label>
-                <input v-model.number="hit4" type="number" min="0" max="1000" step="1" class="form-control">
-            </div>
-            <div class="form-group">
-                <label for="formGroupExampleInput2">四死球数</label>
-                <input v-model.number="fourDeadBalls" type="number" min="0" max="1000" step="1" class="form-control">
-            </div>
-            <div class="form-group">
-                <label for="formGroupExampleInput2">犠飛数</label>
-                <input v-model.number="sacrificeFly" type="number" min="0" max="1000" step="1" class="form-control">
-            </div>
-
-            <h2>計算結果</h2>
-
-            <table class="table table-striped">
-                <tr>
-                    <td>ヒット数</td><td>{{ hitNum }}</td>
-                </tr>
-                <tr>
-                    <td>打率</td><td>{{ average }}</td>
-                </tr>
-                <tr>
-                    <td>出塁率</td><td>{{ onBasePercentage }}</td>
-                </tr>
-                <tr>
-                    <td>長打率</td><td>{{ sluggingPercentage }}</td>
-                </tr>
-                <tr>
-                    <td>OPS</td><td>{{ ops }}</td>
-                </tr>
-            </table>
-
         </form>
+
+        <h2>計算結果</h2>
+        <table class="table table-striped">
+            <tr v-for="result in resultList" :key="result">
+                <td>{{ result }}</td>
+                
+                <td v-if="result == 'ヒット数'">{{ hitNum }}</td>
+                <td v-else-if="result == '打率'">{{ average }}</td>
+                <td v-else-if="result == '出塁率'">{{ onBasePercentage }}</td>
+                <td v-else-if="result == '長打率'">{{ sluggingPercentage }}</td>
+                <td v-else-if="result == 'OPS'">{{ ops }}</td>
+            </tr>
+        </table>
     </div>
 </template>
 
@@ -65,7 +33,17 @@ export default {
       hit3: 0,
       hit4: 0,
       fourDeadBalls: 0,
-      sacrificeFly: 0
+      sacrificeFly: 0,
+      formList: [
+            { label: "打数", data: 'bat' },
+            { label: "単打数", data: 'hit1' },
+            { label: "二塁打数", data: 'hit2' },
+            { label: "三塁打数", data: 'hit3' },
+            { label: "本塁打数", data: 'hit4' },
+            { label: "四死球数", data: 'fourDeadBalls' },
+            { label: "犠飛数", data: 'sacrificeFly' },
+        ],
+      resultList: ["ヒット数", "打率", "出塁率", "長打率", "OPS"]
     }
   },
   computed: {
@@ -74,16 +52,31 @@ export default {
       },
       average: function() {
           let average = this.hitNum / this.bat;
-          return average.toFixed(3);
+
+          if (isNaN(average)) {
+            return 0
+          } else {
+              return average.toFixed(3);
+          }
       },
       onBasePercentage: function() {
           let onBasePercentage = (this.hitNum + this.fourDeadBalls + this.sacrificeFly) / (this.bat + this.fourDeadBalls + this.sacrificeFly);
-          return onBasePercentage.toFixed(3);
+
+          if (isNaN(onBasePercentage)) {
+            return 0
+          } else {
+              return onBasePercentage.toFixed(3);
+          }
       },
       sluggingPercentage: function() {
           let baseHit = this.hit1 + (this.hit2 * 2) + (this.hit3 * 3) + (this.hit4 * 4)
           let sluggingPercentage = baseHit / this.bat;
-          return sluggingPercentage.toFixed(3);
+
+          if (isNaN(sluggingPercentage)) {
+            return 0
+          } else {
+            return sluggingPercentage.toFixed(3);
+          }
       },
       ops: function() {
             console.log(typeof(this.sluggingPercentage));
