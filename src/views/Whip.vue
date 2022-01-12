d69470811e24d52fa2885fd7e7c2345dac6668d8
rk <>
2022-01-12 14:27:58 +0900

    key は文字列か数字じゃないとエラーがでるので修正

--- src/views/Whip.vue
+++ src/views/Whip.vue
@@ -13,7 +13,7 @@
 
         <h2>計算結果</h2>
         <table class="table table-striped">
-            <tr v-for="result in resultList" :key="result">
+            <tr v-for="result in resultList" :key="result.label">
                 <td>{{ result.label }}</td>
                 
                 <td v-if="result.label == 'WHIP'">{{ whip }}</td>
