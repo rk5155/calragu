49cd473b812e5a92bd0af616b651ed777ff57275
rk <>
2022-01-25 18:47:44 +0900

    スマホの時、pattern="\d*"で数字キーボード入力させるようにした

--- src/components/Calculation.vue
+++ src/components/Calculation.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="form-group">
         <label for="formGroupExampleInput">{{ list.label }}</label>
-        <input type="number" min="0" max="1000" :step="step" class="form-control" v-model="inputedValue">
+        <input type="number" min="0" max="1000" pattern="\d*" :step="step" class="form-control" v-model="inputedValue">
     </div>
 </template>
 <script>
